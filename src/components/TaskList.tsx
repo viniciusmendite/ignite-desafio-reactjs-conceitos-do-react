6d858c4f67756b073c88bd89863a985c2619f09e
Vinicius Mendite <vinicius.miff@gmail.com>
2021-03-11 13:37:04 -0300

    create new task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,6 +16,18 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle){
+      alert("Title can't be empty!");
+      return;
+    }
+
+    let newTask = {
+      id: Math.floor(Math.random() * 100),
+      title: newTaskTitle,
+      isComplete: false
+    };
+
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
